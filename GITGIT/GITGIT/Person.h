66af1196dd1a3819373810f39952f85f3e38331c
陈锦飞 <chenjinfei@live.com>
2016-07-28 16:34:53 +0800

    first commit

---
+++ GITGIT/GITGIT/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GITGIT
+//
+//  Created by cjf on 7/28/16.
+//  Copyright © 2016 lanoukeji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GITGIT/GITGIT/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GITGIT
+//
+//  Created by cjf on 7/28/16.
+//  Copyright © 2016 lanoukeji. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
