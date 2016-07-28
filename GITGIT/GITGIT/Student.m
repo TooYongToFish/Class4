b5e028912f61971d67019d5d8a2bb86f1e047e36
陈锦飞 <chenjinfei@live.com>
2016-07-28 16:38:37 +0800

    test student class commit

---
+++ GITGIT/GITGIT/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GITGIT
+//
+//  Created by cjf on 7/28/16.
+//  Copyright © 2016 lanoukeji. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
