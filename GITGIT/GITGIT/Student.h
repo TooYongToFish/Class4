d43a7e3ee08a0017c63a7a840409fc27d1ad7582
胡建 <hujianbo@dogs-five.local>
2016-07-28 16:41:47 +0800

    第二次提交

---
+++ GITGIT/GITGIT/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GITGIT
+//
+//  Created by 胡建波 on 16/7/28.
+//  Copyright © 2016年 lanoukeji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
